4bf2f3d61936c0248a59fc5964d88b08e6db9599
Oana Miron <oana9opt@yahoo.com>
2018-03-04 23:32:18 +0100

    Update Cell.h

--- Bug World/Header/Cell.h
+++ Bug World/Header/Cell.h
@@ -1,14 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   Cell.h
- * Author: oana
- *
- * Created on March 2, 2018, 5:20 PM
+ * Class Cell
  */
 
 #ifndef CELL_H

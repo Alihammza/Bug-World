f1c7459ba3786592b339c2b408e46bbbbf899ca1
Oana Miron <oana9opt@yahoo.com>
2018-03-04 23:30:14 +0100

    Update headers.h

--- Bug World/Header/headers.h
+++ Bug World/Header/headers.h
@@ -1,26 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   headers.h
- * Author: oana
- *
- * Created on March 2, 2018, 7:01 PM
+ * Header file linking all extra classes
  */
 
 #include "tcolor.h"
 #include "tdir.h"
-#include "Bug.h"
-#include "Cell.h"
-#include "Marker.h"
-#include "World.h"
 #include "tcondition.h"
 #include "tleftright.h"
 #include "tmark.h"
 #include "tposition.h"
 #include "tsensedir.h"
 #include "tstate.h"
-#include "instruction.h"

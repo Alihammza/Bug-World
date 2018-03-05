82efa86d940b8d4cab6597feba2f5b01b3054849
Oana Miron <oana9opt@yahoo.com>
2018-03-05 19:21:22 +0100

    Update Cell.cpp

--- Bug World/src/Cell.cpp
+++ Bug World/src/Cell.cpp
@@ -1,96 +1,111 @@
-#include "Cell.h"
-#include <cstdlib>
 /*
- * # - obstacle
- * . - clear cell
- * _ black bug home area cell
- * + red bug home area cell
- * 1...9 clear cell containing n food particles
- * 
- * as many bugs as many home area cells
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
+#include <iostream>
+#include "Bug.h"
 
-Cell::Cell(char symb)
+using namespace std;
+int main()
 {
-    switch(symb){
-        case '#' :
-            obstructed=1;
-            food=0;
-            home_black_bug=0;
-            home_red_bug=0;
-            break;
-        case '.':
-            obstructed=0;
-            food=0;
-            home_black_bug=0;
-            home_red_bug=0;
-            break;
-        case '_':
-            obstructed=1;
-            food=0;
-            home_black_bug=1;
-            home_red_bug=0;
-            break;
-        case '+':
-            obstructed=1;
-            food=0;
-            home_black_bug=0;
-            home_red_bug=1;
-            break;
-        default:
-            if(symb-'0'<=9&&symb-'0'>=0)
-            {
-                obstructed=0;
-                food=symb-'0';
-                home_black_bug=0;
-                home_red_bug=0;
-            }
-            else
-            {
-                throw "Invalid cell specifier passed\n";
-            }
-            break;
+    /*invalid color*/
+    try{
+        Bug b(2,1,14);
     }
-    mark=NULL;
-    occupant=NULL;
-}
-    
-Bug* Cell::get_occupant()
-{
-    return occupant;
-}
-    
-void Cell::set_occupant(Bug new_occupant)
-{
-    occupant=new Bug(new_occupant);
-}
-    
-int Cell::get_food()
-{
-    return food;
-}
-    
-void Cell::set_food(int new_food)
-{
-    if(food<0)
+    catch(char const* e)
     {
-        throw "The amount of food can't be negative\n";
+        cout<<e<<endl;
     }
-    food=new_food;
-}
     
-bool Cell::get_obstructed()
-{
-    return obstructed;
-}
+    /*invalid resting time*/
+    try
+    {
+        Bug b(1,1,-3);
+    }
+    catch(char const *e)
+    {
+        cout<<e<<endl;
+    }
     
-bool Cell::is_black_home_area()
-{
-    return home_black_bug;
-}
+    Bug b(1,1,14);
     
-bool Cell::is_red_home_area()
-{
-    return home_red_bug;
+    /*invalid direction*/
+    try
+    {
+        b.set_direction(6);
+    }
+    catch(char const *e)
+    {
+        cout<<e<<endl;
+    }
+    
+    /*invalid state*/
+    try
+    {
+        b.set_state(-1);
+    }
+    catch(char const* e)
+    {
+        cout<<e<<endl;
+    }
+    
+    /*invalid food*/
+    try
+    {
+        b.set_food(-15);
+    }
+     catch(char const* e)
+    {
+        cout<<e<<endl;
+    }
+    
+    /*correct resting procedure*/
+    b.start_resting();
+    for(int i=0;i<=14;i++)
+        cout<<"Is rested at time "<<i+1<<": "<<b.rested()<<endl;        
+    cout<<"Still rested "<<b.rested()<<endl;
+    
+    /*Correct color and id getters*/
+    tcolor a=1;
+    if(b.get_color()!=a)
+        cout<<"The color getter doesn't work correctly.\n";
+    
+    if(b.get_progid()!=1)
+        cout<<"The id getter doesn't work correctly.\n";
+    
+    /*getter setter check*/
+    b.set_direction(4);
+    if(b.get_direction()!=4)
+        cout<<"The direction setter doesn't work right\n";
+    
+    b.set_food(1);
+    if(b.get_food()!=1)
+        cout<<"The food setter doesn't work right\n";
+    
+    tposition t(1,2);
+    b.set_position(t);
+    if(b.get_position()!=t)
+        cout<<"The position setter doesn't work right\n";
+    
+    b.set_state(2);
+    if(b.get_state()!=tstate(2))
+        cout<<"The state setter doesn't work right\n";
+
+    /*correct killing procedure*/
+    if(b.is_dead())
+        cout<<"The killing function doesn't work well\n";
+    b.kill();
+    if(!b.is_dead())
+        cout<<"The bug should be dead \n";
+    try
+    {
+        b.kill();
+    }
+    catch(char const* e)
+    {
+        cout<<e<<endl;
+    }
+    return 0;
 }

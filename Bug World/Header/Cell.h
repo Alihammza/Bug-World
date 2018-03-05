ec9fc647308ba2f2e52fd73e8b6467687b8a5831
Oana Miron <oana9opt@yahoo.com>
2018-03-05 19:20:04 +0100

    Update Cell.h

--- Bug World/Header/Cell.h
+++ Bug World/Header/Cell.h
@@ -13,19 +13,21 @@
 
 #ifndef CELL_H
 #define CELL_H
+#include "Bug.h"
+#include "Marker.h"
 class Cell 
 {
 private:
-    Bug occupant;
+    Bug* occupant;
     bool obstructed;
     int food;
     bool home_black_bug;
     bool home_red_bug;
 public:
-    Marker mark;
+    Marker* mark;
     Cell(char symb);
-    ~Cell(){};
-    Bug get_occupant();
+    ~Cell(){delete mark; delete occupant;};
+    Bug* get_occupant();
     void set_occupant(Bug new_occupant);
     int get_food();
     void set_food(int new_food);

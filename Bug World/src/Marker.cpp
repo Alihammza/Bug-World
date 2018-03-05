cbc001985d8ed65cee31ac7c2de283682be98c67
Oana Miron <oana9opt@yahoo.com>
2018-03-05 17:44:01 +0100

    Update Marker.cpp

--- Bug World/src/Marker.cpp
+++ Bug World/src/Marker.cpp
@@ -14,6 +14,7 @@
 #include "Marker.h"
 
 Marker::Marker() {
+	this->bits=0;
 }
 
 Marker::Marker(const Marker& orig) {
@@ -30,22 +31,28 @@ int Marker::get_bits(){
     return bits;
 }
 
-void set_marker(tmark mark, tcolor col)
-{
+void set_marker(tmark mark, tcolor col){
+    this->bits = this->bits | 1<<(mark+6*col);
      
 }
  
-void clear_marker(tmark mark, tcolor col)
-{
+void clear_marker(tmark mark, tcolor col){
+	this->bits = this->bits & ~1<<(mark+6*col);
      
 }
  
-bool check_marker(tmark mark, tcolor col)
-{
-     
+bool check_marker(tmark mark, tcolor col){
+    return this->bits & 1<<(mark+6*col);
+	    
 }
    
-bool check_other_marker(tcolor col)
-{
+bool check_other_marker(tcolor col){
+	for (int i = 0; i < 6; i++){
+            if (this->bits & 1<<(i + 6*col)){
+                return true;
+            }
+        }
+    return false;
+	
 }
-}
+
